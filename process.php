562c945aea22e4c57bc8335bafe1301345258266
i0uthis <John.Narte@edu.sait.ca>
2023-07-11 12:42:04 -0600

    Update process.php

--- process.php
+++ process.php
@@ -6,17 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$message = $_POST["message"];
 	
 	//string to save to form data
-	$formData = "Name: $name\nEmail: $email\nMessage: $message\n";
+	$formData = "\nName: $name\nEmail: $email\nMessage: $message\n";
 	
 	$filePath ="form_data.txt";
 	
 	//save contents to form data
 	if(file_put_contents($filePath, $formData, FILE_APPEND)){
 		//data saved
-		echo "Message recieved";
+		echo "Message recieved\n";
 	} else {
 		//failed save
-		echo "Message lost";
+		echo "Message lost\n";
 	}
 	
 	
