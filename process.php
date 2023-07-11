20e01b5ac3ebc2ed02d7db5ed9dc8cd8f22c183a
i0uthis <John.Narte@edu.sait.ca>
2023-07-11 12:19:19 -0600

    process.php update

--- process.php
+++ process.php
@@ -3,8 +3,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	//retrieve data
 	$name = $_POST["name"];
 	$email = $_POST["email"];
+	$message = $_POST["message"];
 	
-	//insert data validation here
+	//string to save to form data
+	$formData = "Name: $name\nEmail: $email\nMessage: $message\n";
+	
+	$filePath ="form_data.txt";
+	
+	//save contents to form data
+	if(file_put_contents($filePath, $formData, FILE_APPEND)){
+		//data saved
+		echo "Message recieved";
+	} else {
+		//failed save
+		echo "Message lost";
+	}
+	
 	
 	//display
 	echo "Name: " . $name . "<br>";
