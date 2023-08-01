124441c9dba85a87aca701328a928e2595e382b0
sam2795 <samuel2795@hotmail.com>
2023-08-01 14:18:18 -0600

    Sam

--- process.php
+++ process.php
@@ -1,28 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-	//retrieve data
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
-	
-	//string to save to form data
-	$formData = "\nName: $name\nEmail: $email\nMessage: $message\n";
-	
-	$filePath ="form_data.txt";
-	
-	//save contents to form data
-	if(file_put_contents($filePath, $formData, FILE_APPEND)){
-		//data saved
-		echo "Message recieved\n";
-	} else {
-		//failed save
-		echo "Message lost\n";
-	}
-	
-	
-	//display
-	echo "Name: " . $name . "<br>";
-	echo "Email: " . $email . "<br>";
-	
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve data and sanitize it to prevent potential security issues
+    $name = isset($_POST["name"]) ? htmlspecialchars($_POST["name"]) : "";
+    $email = isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : "";
+    $message = isset($_POST["message"]) ? htmlspecialchars($_POST["message"]) : "";
+
+    // Validate the data (you can add more validation checks as needed)
+    if (empty($name) || empty($email) || empty($message)) {
+        // Handle empty form fields
+        echo "Please fill out all the required fields.";
+        exit; // Stop execution to prevent saving empty data and further processing
+    }
+
+    // String to save form data
+    $formData = "Name: $name\nEmail: $email\nMessage: $message\n";
+
+    $filePath = "form_data.txt";
+
+    // Save contents to form data file
+    if (file_put_contents($filePath, $formData, FILE_APPEND)) {
+        // Data saved successfully
+        echo "Message received\n";
+    } else {
+        // Failed to save data
+        echo "Failed to save the message.\n";
+    }
+
+    // Display the submitted data
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $email . "<br>";
+    echo "Message: " . $message . "<br>";
 }
 ?>
